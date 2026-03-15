4c6ade2dfb85415fd1c4a4a24be6629c22ec3002
Korven Lab <korvenlab.contato@gmail.com>
2026-03-15 08:40:31 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ app.use(cors({
     'http://localhost:3000' 
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type']
+  allowedHeaders: ['Content-Type','Authorization']
 }));
 
 app.use(express.json());
